aef9a67a9c2d47342b77376e136ad6a146de6f32
Jonathan Sick <jonathansick@mac.com>
2015-01-28 11:48:37 -0500

    ImageLog as a dict and mongodb query intro

---
+++ imagelog_dict.py
@@ -0,0 +1,7 @@
+{"944588": {"OBJECT": "M31-1",
+            "FILTER": "J",
+            "EXPTIME": 40.},
+ "944588": {"OBJECT": "M31-sky-28",
+            "kind": "sky",
+            "FILTER": "J",
+            "EXPTIME": 40.}}
---
+++ mongodb_queries.py
@@ -0,0 +1,3 @@
+find({"OBJECT": "M31-1"})
+find({"OBJECT": "M31-1", "FILTER": "J"})
+find({"OBJECT": "M31-1", "FILTER": {"$in": ["J", "Ks"]}})
