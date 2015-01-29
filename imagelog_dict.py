4df98fe577f5b7778864e05b896868ddea49bbc6
Jonathan Sick <jonathansick@mac.com>
2015-01-29 15:04:46 -0500

    Use real image log data

--- imagelog_dict.py
+++ imagelog_dict.py
@@ -1,7 +1,23 @@
-{"944588": {"OBJECT": "M31-1",
-            "FILTER": "J",
-            "EXPTIME": 40.},
- "944588": {"OBJECT": "M31-sky-28",
-            "kind": "sky",
-            "FILTER": "J",
-            "EXPTIME": 40.}}
+{"933438": {
+    u'OBJECT': u'M31-1',
+    u'FILTER': u'Ks',
+    u'MJDATE': 54317.4610543,
+    u's_path': u'/Volumes/Beeblebrox/data/wircam/07bc20/933438s.fits',
+    u'KIND': 'sci',
+    u'footprint': [[11.45251, 42.07619],
+                   [11.93747, 42.07619],
+                   [11.93747, 42.43618],
+                   [11.45251, 42.43618]],
+    ...,
+    u'nightset': {u'calibrated_path': u'nightset/calibrated/933438.fits',
+                  u'calibrated_weight_path': u'nightset/calibrated/933438.wght.fits.fz',
+                  u'paired_sky': {u'distance': 0.978,
+                                  u'latency': 129.98,
+                                  u'level': {u'1': 9668.72,
+                                             u'2': 9676.98,
+                                             u'3': 9698.52,
+                                             u'4': 9698.59}},
+                  ...}
+    ...}
+ "933439": {...}
+}
