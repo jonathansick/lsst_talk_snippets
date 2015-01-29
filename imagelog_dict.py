de7f98ac4857e2417602f04b11f025a10672de69
Jonathan Sick <jonathansick@mac.com>
2015-01-29 18:10:32 -0500

    Improve imagelog_dict.py formatting

--- imagelog_dict.py
+++ imagelog_dict.py
@@ -8,7 +8,6 @@
                    [11.93747, 42.07619],
                    [11.93747, 42.43618],
                    [11.45251, 42.43618]],
-    ...,
     u'nightset': {u'calibrated_path': u'nightset/calibrated/933438.fits',
                   u'calibrated_weight_path': u'nightset/calibrated/933438.wght.fits.fz',
                   u'paired_sky': {u'distance': 0.978,
@@ -16,8 +15,7 @@
                                   u'level': {u'1': 9668.72,
                                              u'2': 9676.98,
                                              u'3': 9698.52,
-                                             u'4': 9698.59}},
-                  ...}
-    ...}
- "933439": {...}
+                                             u'4': 9698.59}}, ...},
+    ...},
+ "933439": {...},
 }
