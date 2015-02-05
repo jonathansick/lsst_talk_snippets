4014f9a0c0ee8b5da6f7409e94d94374b44d9783
Jonathan Sick <jonathansick@mac.com>
2015-02-05 16:52:02 -0500

    Add embedded doc example

---
+++ mongodb_embedded_doc.py
@@ -0,0 +1,11 @@
+{u'_id': u"933438",
+ 'OBJECT': u'M31-1',
+ 'FILTER': u'Ks',
+ 'queue_flat': {'flattened_zp': '...',
+                'zp': {1: ..., 2: ...,},
+                ...},
+ 'night_flat': {'flattened_zp': '...',
+                'zp': {1: ..., 2: ...,},
+                 ...},
+ ...
+}
